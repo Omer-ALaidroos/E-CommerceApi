653c21a1b3d1e290f1de5adbf779563f5f434de1
Omer-Alaidroos <2020omerahmed2020@gmail.com>
2026-05-03 14:57:51 +0300

    Fix the problrm of create Cart and CartItem classes

---
+++ eCommerceApp.Domain/Entities/Cart/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Domain.Entities.Cart
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public bool IsCheckedOut { get; set; } 
+        public DateTime CreatedAt { get; set; }
+
+        public List<CartItem> Items { get; set; }
+    }
+}
